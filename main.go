60f8b56ed80efb10b8dc05019742dfaba1366c06
emilianoglobalc <135754496+emilianoglobalc@users.noreply.github.com>
2023-09-09 23:42:03 -0600

    Commit

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ func Router() (http.Handler, *cors.Cors) {
 
 	r.HandleFunc("/api/v1/cuenta/SignUp", controladores.SignUp).Methods(http.MethodPost)
 	r.HandleFunc("/api/v1/cuenta/SignIn", controladores.SignIn).Methods(http.MethodPost)
+	r.HandleFunc("/api/v1/cuenta/RecuperaContrasena", controladores.RecuperaContrasena).Methods(http.MethodPost)
 
 	r.HandleFunc("/api/v1/prueba/saludo", controladores.Prueba).Methods(http.MethodGet)
 
