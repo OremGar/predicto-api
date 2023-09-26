af9ee469481a7ebefed1bbd80900a9d04ae17be8
emilianoglobalc <135754496+emilianoglobalc@users.noreply.github.com>
2023-09-26 14:02:49 -0600

    Commit

--- main.go
+++ main.go
@@ -24,8 +24,8 @@ func Router() (http.Handler, *cors.Cors) {
 	r.HandleFunc("/api/v1/cuenta/SignUp", controladores.SignUp).Methods(http.MethodPost)
 	r.HandleFunc("/api/v1/cuenta/SignIn", controladores.SignIn).Methods(http.MethodPost)
 	r.HandleFunc("/api/v1/cuenta/RecuperaContrasena", controladores.RecuperaContrasena).Methods(http.MethodPost)
+	r.HandleFunc("/api/v1/cuenta/NuevaContrasena", controladores.NuevaContrasena).Methods(http.MethodPut)
 	r.HandleFunc("/api/v1/cuenta/ValidaOTP/{codigo:[0-9]+}", controladores.ValidaOTP).Methods(http.MethodGet)
-
 	r.HandleFunc("/api/v1/prueba/saludo", controladores.Prueba).Methods(http.MethodGet)
 
 	wrappedMux = middlewares.ValidarToken(r.ServeHTTP)
